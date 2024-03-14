b0358aee9bf499ce924f9449a15b93c607905a9f
vier <oliviervroege@hotmail.com>
2024-03-14 11:00:51 +0100

    second commit :)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,7 @@
     import "../app.css";
   </script>
   
-  <slot />
+  <div class="text-xl mb-8 w-full bg-gradient-to-r from-cyan-800 to-indigo-900 flex justify-center p-4 items-center border-b-2 border-white text-white">
+    QR-Code Generator
+  </div>
+    <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,41 @@
-<form class="w-full grid grid-cols-2" action="GET">
-    <input type="text" placeholder="name" class="bg-grey text-white border-2 border-blue-800">
-</form>
+<script>
+    import { qr } from '@svelte-put/qr/img';
+
+    const generateQR = () => {
+
+    }
+
+    const PDFExport = () => {
+
+    }
+</script>
+
+<div class="flex-col">
+    <form class=" flex justify-center my-12" action="GET">
+        <div class="grid grid-cols-2 gap-4">
+            <input type="text" placeholder="Name" class="bg-gray-700 text-white border-2 border-white rounded-md p-2">
+            <input type="text" placeholder="ID Nr." class="bg-gray-700 text-white border-2 border-white rounded-md p-2">    
+            <input type="text" placeholder="Role" class="bg-gray-700 text-white border-2 border-white rounded-md p-2">
+            <button class="bg-gray-700 text-white border-2 border-white rounded-md p-2" on:click={() => generateQR()}>Generate</button>
+        </div>
+    </form>
+    <div class=" flex justify-center">
+        <div class="h-48 w-48 bg-white flex justify-center  place-items-center">            
+            <img
+                use:qr={{
+                data: 'B, Hans, Ruedi, 123123123',
+                logo: 'https://svelte-put.vnphanquang.com/images/svelte-put-logo.svg',
+                shape: 'circle',
+                anchorInnerFill: 'black',
+                anchorOuterFill: 'black',
+                moduleFill: 'black',
+                }}
+                alt="qr"
+            />
+            
+        </div>
+    </div>
+    <div class=" flex justify-center">
+        <button class="bg-white m-8 p-2 rounded-md" on:click={() => PDFExport()}>Export PDF</button>
+    </div>
+</div>
