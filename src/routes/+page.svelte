777b0a09080ed13a66cea8cf6855c70976d3b618
vier <oliviervroege@hotmail.com>
2024-03-21 10:37:21 +0100

    writable QR

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+    import { writable } from 'svelte/store';
     import { qr } from '@svelte-put/qr/img';
 
-    const generateQR = () => {
-
-    }
+    let userData = writable({
+        name: "",
+        IDNr: "",
+        Role: ""
+    })
 
     const PDFExport = () => {
 
@@ -11,19 +14,18 @@
 </script>
 
 <div class="flex-col">
-    <form class=" flex justify-center my-12" action="GET">
+    <div class=" flex justify-center my-12">
         <div class="grid grid-cols-2 gap-4">
-            <input type="text" placeholder="Name" class="bg-gray-700 text-white border-2 border-white rounded-md p-2">
-            <input type="text" placeholder="ID Nr." class="bg-gray-700 text-white border-2 border-white rounded-md p-2">    
-            <input type="text" placeholder="Role" class="bg-gray-700 text-white border-2 border-white rounded-md p-2">
-            <button class="bg-gray-700 text-white border-2 border-white rounded-md p-2" on:click={() => generateQR()}>Generate</button>
+            <input type="text" placeholder="Name" class="bg-gray-700 text-white border-2 border-white rounded-md p-2" bind:value={$userData.name}>
+            <input type="text" placeholder="ID Nr." class="bg-gray-700 text-white border-2 border-white rounded-md p-2" bind:value={$userData.IDNr}>    
+            <input type="text" placeholder="Role" class="bg-gray-700 text-white border-2 border-white rounded-md p-2" bind:value={$userData.Role}>
         </div>
-    </form>
+    </div>
     <div class=" flex justify-center">
         <div class="h-48 w-48 bg-white flex justify-center  place-items-center rounded-md">            
             <img
                 use:qr={{
-                data: 'B, Hans, Ruedi, 123123123',
+                data: `${$userData.name} ${$userData.IDNr} ${$userData.Role}`,
                 logo: 'https://svelte-put.vnphanquang.com/images/svelte-put-logo.svg',
                 shape: 'circle',
                 anchorInnerFill: 'black',
@@ -32,7 +34,6 @@
                 }}
                 alt="qr"
             />
-            
         </div>
     </div>
     <div class=" flex justify-center">
