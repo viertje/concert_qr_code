9481b00684c88c1bc7aa9161637d1c55755c3464
Seismix <11737685+Seismix@users.noreply.github.com>
2024-03-28 09:16:40 +0100

    Prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
     import "../app.css";
-  </script>
-  
-  <div class="text-xl mb-8 w-full bg-gradient-to-r from-cyan-800 to-indigo-900 flex justify-center p-4 items-center border-b-2 border-white text-white">
+</script>
+
+<div
+    class="text-xl mb-8 w-full bg-gradient-to-r from-cyan-800 to-indigo-900 flex justify-center p-4 items-center border-b-2 border-white text-white"
+>
     QR-Code Generator
-  </div>
-    <slot />
+</div>
+<slot />
